11c1e03bd0d53eaef05315b00f6dc8f8f378925e
Martí Corbalan <martilluc01@gmail.com>
2024-03-19 12:32:57 +0100

    second commit

--- assets/info.js
+++ assets/info.js
@@ -1,6 +1,20 @@
-fetch('https://swapi.info/api')
-    .then((response) => response.json())
-    .then((json) => console.json(json)[0])
-    .catch((error) => console.error.json(error))
+let contentPath = './assets/contenidoCAT.json';
+fetch(contentPath)
+    .then(response => response.json())
+    .then(data => {
+        createMenu(data.menu);
+    })
 
 // que los LI salgan por js json
+
+function createMenu(menu){
+    const navbar = document.getElementById("nav");
+    const ul = document.createElement('ul');
+    navBar.innerHTML = "";
+    ul.innerHTML = "";
+   for(let i=0; i>=menu.lenght; i++ ){
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = menu[i]
+   }
+}
